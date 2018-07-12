0205b1fb968a12227b235f9a6d3d0b3a31209346
nhlong2 <nhlong2@tma.com.vn>
2018-07-12 18:02:47 +0700

    Add bootstrap library

--- routes/products.js
+++ routes/products.js
@@ -3,7 +3,9 @@ var router = express.Router();
 const productController = require('../server/controllers').Product;
 
 /* GET users listing. */
-router.get('/', productController.list);
+router.get('/', function (req, res, next) {
+  res.render('product');
+});
 
 router.post('/add', productController.create);
 
